952905894c3207095969171270e5ff9590f46ad7
Stephane Cooperstein (CMS <stephane@login05.hep.wisc.edu>
2014-01-22 03:41:18 -0600

    reconfigure run setup

--- zh/run.sh
+++ zh/run.sh
@@ -6,13 +6,21 @@ set -o errexit
 
 source jobid.sh
 
-export jobid=$jobid7
-rake fakerates
-rake fits
+#export jobid=$jobid7
+#rake fakerates
+#rake fits
 #rake analyzezh
 
 export jobid=$jobid8
 rake fakerates
 rake fits
-#rake analyzezh
+rake analyzezh
+#rake plots
+#rake mmtt
+#rake mmtt_ggzz
+#rake eeem_ggzz
+#rake data_only_mmmt
+#rake eett
+#rake mmtt
+#rake mmtt_signal_only
 
