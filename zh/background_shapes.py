c49e8f7d626531cd542ebfad838ec06dee1b0326
Tyler Ruggles <truggles@wisc.edu>
2015-01-25 20:27:21 -0600

    Incorporated tdrStyle

--- zh/background_shapes.py
+++ zh/background_shapes.py
@@ -1,7 +1,10 @@
+#!/usr/bin/env python
 from ROOT import gROOT
 from ROOT import gStyle
 import ROOT
 import os
+import pyplotter.plot_functions as plotter
+
 
 blind = True
 A300Scaling = 20
@@ -70,60 +73,62 @@ run_map = { "AllChannels" : (AllChannels, ('Mass', 'Visible Mass_{l^{+}l^{-}#tau
                                           ('Pt', 'Lepton2 Pt', 1),
                                           ('Pt', 'Vector Sum Pt_{#tau^{+}#tau^{-}}', 'h'),
                                           ('Pt', 'Vector Sum Pt_{l^{+}l^{-}}', 'z'), ),
-#            "ZChannelsEE" : (ZChannelsEE, ('Mass', 'Visible Mass_{l^{+}l^{-}#tau^{+}#tau^{-}}', 'all'),
-#                                          ('Mass', 'Mass_{l^{+}l^{-}}', 'z'),
-#                                          ('mva_metEt', 'mva metEt', 'all'),
-#                                          ('A_SVfitMass', 'Mass_{l^{+}l^{-}#tau^{+}#tau^{-}}', 'all'),
-#                                          ('Pt', 'Lepton1 Pt', 0),
-#                                          ('Pt', 'Lepton2 Pt', 1),
-#                                          ('Pt', 'Vector Sum Pt_{l^{+}l^{-}}', 'z'), ),
-#            "ZChannelsMM" : (ZChannelsMM, ('Mass', 'Visible Mass_{l^{+}l^{-}#tau^{+}#tau^{-}}', 'all'),
-#                                          ('Mass', 'Mass_{l^{+}l^{-}}', 'z'),
-#                                          ('mva_metEt', 'mva metEt', 'all'),
-#                                          ('A_SVfitMass', 'Mass_{l^{+}l^{-}#tau^{+}#tau^{-}}', 'all'),
-#                                          ('Pt', 'Lepton1 Pt', 0),
-#                                          ('Pt', 'Lepton2 Pt', 1),
-#                                          ('Pt', 'Vector Sum Pt_{l^{+}l^{-}}', 'z'), ),
-#            "hChannelsEM" : (hChannelsEM, ('Mass', 'Visible Mass_{l^{+}l^{-}#tau^{+}#tau^{-}}', 'all'),
-#                                          ('Mass', 'Visible Mass_{#tau^{+}#tau^{-}}', 'h'),
-#                                          ('LT_Higgs', 'L_{T} #tau1 #tau2', 'all'),
-#                                          ('mva_metEt', 'mva metEt', 'all'),
-#                                          ('A_SVfitMass', 'Mass_{l^{+}l^{-}#tau^{+}#tau^{-}}', 'all'),
-#                                          ('SVfitMass', 'Mass_{#tau^{+}#tau^{-}}', 'h'),
-#                                          ('Pt', '#tau1 Pt', 2),
-#                                          ('Pt', '#tau2 Pt', 3),
-#                                          ('Pt', 'Vector Sum Pt_{#tau^{+}#tau^{-}}', 'h'), ),
-#            "hChannelsET" : (hChannelsET, ('Mass', 'Visible Mass_{l^{+}l^{-}#tau^{+}#tau^{-}}', 'all'),
-#                                          ('Mass', 'Visible Mass_{#tau^{+}#tau^{-}}', 'h'),
-#                                          ('LT_Higgs', 'L_{T} #tau1 #tau2', 'all'),
-#                                          ('mva_metEt', 'mva metEt', 'all'),
-#                                          ('A_SVfitMass', 'Mass_{l^{+}l^{-}#tau^{+}#tau^{-}}', 'all'),
-#                                          ('SVfitMass', 'Mass_{#tau^{+}#tau^{-}}', 'h'),
-#                                          ('Pt', '#tau1 Pt', 2),
-#                                          ('Pt', '#tau2 Pt', 3),
-#                                          ('Pt', 'Vector Sum Pt_{#tau^{+}#tau^{-}}', 'h'), ),
-#            "hChannelsMT" : (hChannelsMT, ('Mass', 'Visible Mass_{l^{+}l^{-}#tau^{+}#tau^{-}}', 'all'),
-#                                          ('Mass', 'Visible Mass_{#tau^{+}#tau^{-}}', 'h'),
-#                                          ('LT_Higgs', 'L_{T} #tau1 #tau2', 'all'),
-#                                          ('mva_metEt', 'mva metEt', 'all'),
-#                                          ('A_SVfitMass', 'Mass_{l^{+}l^{-}#tau^{+}#tau^{-}}', 'all'),
-#                                          ('SVfitMass', 'Mass_{#tau^{+}#tau^{-}}', 'h'),
-#                                          ('Pt', '#tau1 Pt', 2),
-#                                          ('Pt', '#tau2 Pt', 3),
-#                                          ('Pt', 'Vector Sum Pt_{#tau^{+}#tau^{-}}', 'h'), ),
-#            "hChannelsTT" : (hChannelsTT, ('Mass', 'Visible Mass_{l^{+}l^{-}#tau^{+}#tau^{-}}', 'all'),
-#                                          ('Mass', 'Visible Mass_{#tau^{+}#tau^{-}}', 'h'),
-#                                          ('LT_Higgs', 'L_{T} #tau1 #tau2', 'all'),
-#                                          ('mva_metEt', 'mva metEt', 'all'),
-#                                          ('A_SVfitMass', 'Mass_{l^{+}l^{-}#tau^{+}#tau^{-}}', 'all'),
-#                                          ('SVfitMass', 'Mass_{#tau^{+}#tau^{-}}', 'h'),
-#                                          ('Pt', '#tau1 Pt', 2),
-#                                          ('Pt', '#tau2 Pt', 3),
-#                                          ('Pt', 'Vector Sum Pt_{#tau^{+}#tau^{-}}', 'h'), ),
+            "ZChannelsEE" : (ZChannelsEE, ('Mass', 'Visible Mass_{l^{+}l^{-}#tau^{+}#tau^{-}}', 'all'),
+                                          ('Mass', 'Mass_{l^{+}l^{-}}', 'z'),
+                                          ('mva_metEt', 'mva metEt', 'all'),
+                                          ('A_SVfitMass', 'Mass_{l^{+}l^{-}#tau^{+}#tau^{-}}', 'all'),
+                                          ('Pt', 'Lepton1 Pt', 0),
+                                          ('Pt', 'Lepton2 Pt', 1),
+                                          ('Pt', 'Vector Sum Pt_{l^{+}l^{-}}', 'z'), ),
+            "ZChannelsMM" : (ZChannelsMM, ('Mass', 'Visible Mass_{l^{+}l^{-}#tau^{+}#tau^{-}}', 'all'),
+                                          ('Mass', 'Mass_{l^{+}l^{-}}', 'z'),
+                                          ('mva_metEt', 'mva metEt', 'all'),
+                                          ('A_SVfitMass', 'Mass_{l^{+}l^{-}#tau^{+}#tau^{-}}', 'all'),
+                                          ('Pt', 'Lepton1 Pt', 0),
+                                          ('Pt', 'Lepton2 Pt', 1),
+                                          ('Pt', 'Vector Sum Pt_{l^{+}l^{-}}', 'z'), ),
+            "hChannelsEM" : (hChannelsEM, ('Mass', 'Visible Mass_{l^{+}l^{-}#tau^{+}#tau^{-}}', 'all'),
+                                          ('Mass', 'Visible Mass_{#tau^{+}#tau^{-}}', 'h'),
+                                          ('LT_Higgs', 'L_{T} #tau1 #tau2', 'all'),
+                                          ('mva_metEt', 'mva metEt', 'all'),
+                                          ('A_SVfitMass', 'Mass_{l^{+}l^{-}#tau^{+}#tau^{-}}', 'all'),
+                                          ('SVfitMass', 'Mass_{#tau^{+}#tau^{-}}', 'h'),
+                                          ('Pt', '#tau1 Pt', 2),
+                                          ('Pt', '#tau2 Pt', 3),
+                                          ('Pt', 'Vector Sum Pt_{#tau^{+}#tau^{-}}', 'h'), ),
+            "hChannelsET" : (hChannelsET, ('Mass', 'Visible Mass_{l^{+}l^{-}#tau^{+}#tau^{-}}', 'all'),
+                                          ('Mass', 'Visible Mass_{#tau^{+}#tau^{-}}', 'h'),
+                                          ('LT_Higgs', 'L_{T} #tau1 #tau2', 'all'),
+                                          ('mva_metEt', 'mva metEt', 'all'),
+                                          ('A_SVfitMass', 'Mass_{l^{+}l^{-}#tau^{+}#tau^{-}}', 'all'),
+                                          ('SVfitMass', 'Mass_{#tau^{+}#tau^{-}}', 'h'),
+                                          ('Pt', '#tau1 Pt', 2),
+                                          ('Pt', '#tau2 Pt', 3),
+                                          ('Pt', 'Vector Sum Pt_{#tau^{+}#tau^{-}}', 'h'), ),
+            "hChannelsMT" : (hChannelsMT, ('Mass', 'Visible Mass_{l^{+}l^{-}#tau^{+}#tau^{-}}', 'all'),
+                                          ('Mass', 'Visible Mass_{#tau^{+}#tau^{-}}', 'h'),
+                                          ('LT_Higgs', 'L_{T} #tau1 #tau2', 'all'),
+                                          ('mva_metEt', 'mva metEt', 'all'),
+                                          ('A_SVfitMass', 'Mass_{l^{+}l^{-}#tau^{+}#tau^{-}}', 'all'),
+                                          ('SVfitMass', 'Mass_{#tau^{+}#tau^{-}}', 'h'),
+                                          ('Pt', '#tau1 Pt', 2),
+                                          ('Pt', '#tau2 Pt', 3),
+                                          ('Pt', 'Vector Sum Pt_{#tau^{+}#tau^{-}}', 'h'), ),
+            "hChannelsTT" : (hChannelsTT, ('Mass', 'Visible Mass_{l^{+}l^{-}#tau^{+}#tau^{-}}', 'all'),
+                                          ('Mass', 'Visible Mass_{#tau^{+}#tau^{-}}', 'h'),
+                                          ('LT_Higgs', 'L_{T} #tau1 #tau2', 'all'),
+                                          ('mva_metEt', 'mva metEt', 'all'),
+                                          ('A_SVfitMass', 'Mass_{l^{+}l^{-}#tau^{+}#tau^{-}}', 'all'),
+                                          ('SVfitMass', 'Mass_{#tau^{+}#tau^{-}}', 'h'),
+                                          ('Pt', '#tau1 Pt', 2),
+                                          ('Pt', '#tau2 Pt', 3),
+                                          ('Pt', 'Vector Sum Pt_{#tau^{+}#tau^{-}}', 'h'), ),
 }
 
 for key in run_map.keys():
-    print key
+    if key == "AllChannels": pass
+    else: break
+    #print key
     nPlots = len( run_map[key] )
     for i in range(1, nPlots):
         variable = run_map[key][i][0]
@@ -136,6 +141,7 @@ for key in run_map.keys():
             varBin = variables_map[variable][0]
         my_total = ROOT.THStack("my_total", "CMS Preliminary, Red + Irr bgk & Data, 19.7 fb^{-1} at S=#sqrt{8} TeV")
         my_shapes = ROOT.TFile("results/2014-02-28_8TeV_Ntuples-v2/cards/shapes.root", "r")
+        #my_shapes = ROOT.TFile("shapes_Nov18_0.root", "r")
         my_data = ROOT.TH1F("my_data", "Data", varBin, 0, varRange)
         my_ZH_ww125 = ROOT.TH1F("my_ZH_ww125", "ZH_ww125", varBin, 0, varRange)
         my_ZH_tt125 = ROOT.TH1F("my_ZH_tt125", "ZH_tt125", varBin, 0, varRange)
@@ -149,32 +155,24 @@ for key in run_map.keys():
         my_A300 = ROOT.TH1F("my_A300", "%i x A300, xsec=1fb" % A300Scaling, varBin, 0, varRange)
         
         for sample in ['ZH_ww125', 'ZH_tt125', 'TTZ', 'GGToZZ2L2L', 'ZZ', 'Zjets', 'data_obs', 'AZh300', 'ZZZ', 'WZZ', 'WWZ']:
-            print sample
+            #print sample
             my_red_combined = ROOT.THStack("%s combined" % sample, "%s combined" % sample)
         
             for channel in run_map[key][0]:
-                #print channel
                 if run_map[key][i][2] == "all":
-                    #print "no var"
                     if variable == 'A_SVfitMass': sampVar = sample
                     else: sampVar = sample + "_" + variable
                 elif run_map[key][i][2] == "z":
-                    #print "Z"
                     first = products_map[channel][0]
                     second = products_map[channel][1]
                     sampVar = "%s_%s_%s_%s" % (sample, first, second, variable)
-                    #print sampVar
                 elif run_map[key][i][2] == "h":
-                    #print "h"
                     first = products_map[channel][2]
                     second = products_map[channel][3]
                     sampVar = "%s_%s_%s_%s" % (sample, first, second, variable)
                 else:
-                    #print "singles"
                     first = products_map[channel][ run_map[key][i][2] ]
                     sampVar = "%s_%s%s" % (sample, first, variable)
-                    #print sampVar
-                #print "sampVar: %s" % sampVar
                 my_red = my_shapes.Get("%s_zh/%s" % (channel, sampVar) )
                 #print "Bin#: %i" % my_red.GetSize()
                 #print "Max: %f" % ((my_red.GetSize() - 2) * my_red.GetBinWidth(1))
@@ -182,7 +180,9 @@ for key in run_map.keys():
                 #except AttributeError:
                 #    print sampVar + 'in channel ' + channel + 'has problems'
                 #    continue
-                c1 = ROOT.TCanvas("c1", "a canvas")
+                ##c1 = ("c1", "a canvas")
+                c1 = plotter.getCanvas()
+                #plotter.setTDRStyle(c1, 19.7, 8, "left") 
                 pad1 = ROOT.TPad("pad1","",0,0,1,1) # compare distributions
                 pad1.Draw()    
                 pad1.cd() 
@@ -191,10 +191,6 @@ for key in run_map.keys():
                 if run_map[key][i][0] == 'Pt': my_red.Rebin(4)
                 if run_map[key][i][0] == 'mva_metEt': my_red.Rebin(2)
                 #if run_map[key][i][0] == 'Mass' and run_map[key][i][2] == 'all': my_red.Rebin(4)
-                #my_red.SetLineColor(ROOT.kRed)
-                #my_red.SetMarkerColor(ROOT.kRed)
-                #my_red.SetFillColor(ROOT.kRed)
-                #my_red.SetFillStyle(1001)
                 my_red.GetXaxis().SetTitle("%s (GeV), %s" % (variable, channel) )
         
                 ''' Set reasonable maximums on histos '''        
@@ -209,18 +205,22 @@ for key in run_map.keys():
                 new_my_red = my_red.Clone()
                 my_red_combined.Add(new_my_red)
             
-            c2 = ROOT.TCanvas("c2", "a canvas")
+            #c2 = ROOT.TCanvas("c2", "a canvas")
+            c2 = plotter.getCanvas()
+            plotter.setTDRStyle(c2, 19.7, 8, "left") 
             pad2 = ROOT.TPad("pad2","",0,0.2,1,1) # compare distributions
             pad2.Draw()
-            
             pad2.cd()
             my_red_combined.GetStack().Last().Draw()
             #my_red_combined.GetStack().Last().GetXaxis().SetTitle("%s (GeV), combined" % variable)
             #pad2.SetGrid() 
             #c2.SaveAs("/afs/hep.wisc.edu/home/truggles/public_html/A_to_Zh_Plots/background_comparisons/%s/combined.png" % sample)
+
+            ROOT.gStyle.SetTitleOffset(1.15, "x")
+            ROOT.gPad.Modified()
+            ROOT.gPad.Update()
             pad2.Close()
             c2.Close()
-        
             gROOT.cd()
         
             if sample == 'data_obs':
@@ -243,10 +243,6 @@ for key in run_map.keys():
                         my_data.SetBinContent(bin, 0)
             if sample == 'AZh300':
                 my_A300.Add( my_red_combined.GetStack().Last().Clone() )
-                #my_A300.SetLineColor(ROOT.kPink+10)
-                #my_A300.SetLineWidth(3)
-                #my_A300.SetFillStyle(3140)
-                #print "A300 Int: %f" % my_A300.Integral()
                 my_A300.Scale( A300Scaling )
             if sample != 'data_obs' and sample != 'AZh300':
                 color = "ROOT.%s" % samples[sample][0]
@@ -262,57 +258,49 @@ for key in run_map.keys():
                 call = "my_%s.Clone()" % sample
                 my_total.Add( eval( call ), "hist" )
         
-        c3 = ROOT.TCanvas("c3", "a canvas")
+        c3 = plotter.getCanvas() # Use Kenneth's canvas setup
         pad5 = ROOT.TPad("pad5","",0,0,1,1) # compare distributions
         pad5.Draw()
         pad5.SetGridy(1)
         pad5.cd()
         my_total.Draw("hist")
-        #my_total.GetYaxis().SetTitle("hello")
         my_total.GetYaxis().SetTitle("Events / %i %s" % ( (variables_map[variable][1]/variables_map[variable][0]), variables_map[variable][3] ) )
         my_total.GetXaxis().SetTitle("%s %s" % (run_map[key][i][1], variables_map[variable][3]) )
         my_A300.SetLineWidth(2)
         my_A300.SetLineColor(ROOT.kOrange+10)
-        #my_A300.SetFillStyle(0)
-        #my_A300.SetFillColor(ROOT.kRed)
         my_A300.Draw("hist same")
-        #my_A300.GetYaxis().SetTitle("Events / %i %s" % ( (variables_map[variable][1]/variables_map[variable][0]), variables_map[variable][3] ) )
-        #my_A300.GetXaxis().SetTitle("%s %s" % (variables_map[variable][2], variables_map[variable][3]) )
         if my_data.GetMaximum() > my_total.GetMaximum():
           my_total.SetMaximum( 1.3 * my_data.GetMaximum() )
         else: my_total.SetMaximum( 1.3 * my_total.GetMaximum() )
         my_A300.SetStats(0)
-        #print "Fill Style: %i" % my_A300.GetFillStyle()
-        #my_total.Draw("hist same")
         #print "My Total Int: %f" % my_total.GetStack().Last().Integral()
         my_data.Draw("e1 same")
         my_data.SetMarkerStyle(21)
         my_data.SetMarkerSize(.8)
-        leg = pad5.BuildLegend(0.60, 0.55, 0.9, 0.9)
+        leg = pad5.BuildLegend(0.60, 0.55, 0.93, 0.89)
         leg.SetMargin(0.3)
         leg.SetFillColor(0)
+        leg.SetBorderSize(0)
         leg.Draw()
-        my_total.SetTitle("CMS Preliminary, Background, Data & MC Signal, 19.7 fb^{-1} at S=#sqrt{8} TeV")
-       
-        #if variables_map[ variable][4] == "h": saveVar = variable +'_h'
-        #if variables_map[ variable][4] == "z": saveVar = variable +'_z'
         fileName = "%s/%s_%s" % ( key, run_map[key][i][2], run_map[key][i][0])
 
-        #saveVar = variable
-        
         #c3.SaveAs("/afs/hep.wisc.edu/home/truggles/public_html/A_to_Zh_Plots/background_comparisons/total_bkg_%s.pdf" % saveVar)
         txtLow = 5
         if run_map[key][i][0] == 'Mass':
             my_total.GetXaxis().SetRange(0, 36)
         if run_map[key][i][0] == 'Mass' and run_map[key][i][2] == "z":
             my_total.GetXaxis().SetRange(5, 15)
-#        if run_map[key][i][0] == 'Pt':
-#          my_total.GetXaxis().Rebin(4)
-#          my_data.Rebin(4)
-#          my_A300.Rebin(4)
-            #txtLow = my_total.GetXaxis().GetBinWidth(1) * 5 + 5
-        pad5.SetLogy()        
-        c3.SaveAs("/afs/hep.wisc.edu/home/truggles/public_html/A_to_Zh_Plots/background_comparisons/%s.png" % fileName)
+        #pad5.SetLogy()        
+        plotter.setTDRStyle(c3, 19.7, 8, "left") 
+
+        ''' Adjusts the location of the X axis title,
+        this is difficult because you can't accest a THStacks'
+        axis titles '''
+        ROOT.gStyle.SetTitleOffset(1.15, "x")
+        ROOT.gPad.Modified()
+        ROOT.gPad.Update()
+
+        c3.SaveAs("/afs/hep.wisc.edu/home/truggles/public_html/A_to_Zh_Plots/background_comparisons/%s.pdf" % fileName)
 #        if run_map[key][i][0] == 'mva_metEt':
 #            pad5.SetLogy()
 #            my_total.SetMinimum( 0.001 )
