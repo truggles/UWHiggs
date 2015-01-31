2e9d0db3a74d16db9b21af579d9ed2e65e24c8cd
Tyler Ruggles <truggles@wisc.edu>
2015-01-30 18:19:35 -0600

    another iteration of variables outputs

--- zh/sync_compare_usesets.py
+++ zh/sync_compare_usesets.py
@@ -1,8 +1,7 @@
 import sys
 
-my_file = open(sys.argv[1], 'r')
-cecile_file = open(sys.argv[2], 'r')
-channel = sys.argv[3]
+my_file = open("uw_data_events.txt", 'r')
+cecile_file = open("list_observed_events.txt", 'r')
 channel_map = { '1' : 'MMTT',
                 '5' : 'EETT',
                 '3' : 'MMMT',
@@ -13,48 +12,35 @@ channel_map = { '1' : 'MMTT',
                 '8' : 'EEEM',
 }
 
-
-print "Now analyzing channel %s" % channel_map[channel]
 my_events = set()
 for line in my_file:
-    evt = line.strip().split(' ')
+    evt = line.strip().split(':')
     #evt.pop()
     #evt.pop()
-    if (evt[0] == channel and evt[0] != '4'):
-      #my_events.add((evt[1],evt[2],evt[3]))
-      #my_events.add((evt[1],evt[2],evt[3],evt[4]))
-      my_events.add((evt[1],evt[2],evt[3],evt[4],evt[5],evt[6],evt[7],evt[8])) # 7 = t2Pt, 8 = t2Eta
-      #my_events.add((evt[1],evt[2],evt[3],evt[5],evt[6],evt[7],evt[8])) # 7 = t2Pt, 8 = t2Eta
-      #my_events.add((evt[1],evt[2],evt[3],evt[4],evt[5],evt[6],evt[7])) # 6 = t2Pt, 7 = t2Eta
-      #my_events.add((evt[1],evt[2],evt[4],evt[5],evt[6],evt[7],evt[8])) # 6 = t2Pt, 7 = t2Eta w/o EventID
-      #my_events.add((evt[1],evt[2],evt[3],round(float(evt[4])),round(float(evt[5]))))
-    elif(evt[0] == channel):
-      #my_events.add((evt[1],evt[2],evt[3]))
-      #my_events.add((evt[1],evt[2],evt[3],evt[4]))
-      my_events.add((evt[1],evt[2],evt[3],evt[4],evt[7],evt[8],evt[5],evt[6]))
-      #my_events.add((evt[1],evt[2],evt[3],evt[7],evt[8],evt[5],evt[6]))
-      #my_events.add((evt[1],evt[2],evt[3],evt[6],evt[7],evt[4],evt[5]))
-      #my_events.add((evt[1],evt[2],evt[6],evt[7],evt[4],evt[5],evt[8])) # w/o EventID
+    if (evt[0] == 'MMEM'):
+      #channel,row.run,row.lumi,(row.evt1*10**5 + row.evt2),row.mva_me    tEt,row.t1Pt,row.t2Pt,( row.SVFit_h_Mass + row.Z_Mass
+      #my_events.add((evt[1],evt[2],evt[3],round( float( evt[6]), 3),round( float( evt[5]), 3),round( float( evt[7]), 3))) # 7 = t2Pt, 8 = t2Eta
+      #my_events.add((evt[1],evt[2],evt[3],round( float( evt[4]), -1),round( float( evt[6]), 0),round( float( evt[5]), 0))) # 7 = t2Pt, 8 = t2Eta
+      my_events.add((evt[0],evt[1],evt[2],evt[3]))
+    else:
+      #my_events.add((evt[1],evt[2],evt[3],round( float( evt[5]), 3),round( float( evt[6]), 3),round( float( evt[7]), 3)))
+      #my_events.add((evt[1],evt[2],evt[3],round( float( evt[4]), -1),round( float( evt[5]), 0),round( float( evt[6]), 0)))
+      my_events.add((evt[0],evt[1],evt[2],evt[3]))
 
-#cecile_file = open("cecile_formatted.txt", "w")
+print len(my_events)
+#print my_events
+
 cecile_events = set()
 for line in cecile_file:
-    evt = line.strip().split(' ')
-    #evt.pop()
-    #evt.pop()
-    # ignore 7 TeV data
-    #if (int(evt[1]) < 190456): continue
-    #print "skipped..."
-    if (evt[0] == channel):
-      #cecile_events.add((evt[1],evt[2],evt[3]))
-      #cecile_events.add((evt[1],evt[2],evt[3],evt[4]))
-      cecile_events.add((evt[1],evt[2],evt[3],evt[4],evt[5],evt[6],evt[7],evt[8]))
-      #cecile_events.add((evt[1],evt[2],evt[3],evt[5],evt[6],evt[7],evt[8]))
-      #cecile_events.add((evt[1],evt[2],evt[3],evt[4],evt[5],evt[6],evt[7]))
-      #cecile_events.add((evt[1],evt[2],evt[4],evt[5],evt[6],evt[7],evt[8])) # w/o EventID
-      #cecile_events.add((evt[1],evt[2],evt[3],round(float(evt[4])),round(float(evt[5]))))
-      #s = evt[0] + ":" + evt[1] + ":" + evt[2] + "\n"
-      #cecile_file.write(s)
+     #channel run:lumi:event mvaMET l1Pt l2Pt l3Pt l4Pt AMass
+     evt = line.strip().split(' ')
+     id = evt[1].split(':')
+     #cecile_events.add((id[0],id[1],id[2],round( float( evt[5]), 3),round( float( evt[6]), 3),round( float( evt[7]), 3)))
+     #cecile_events.add((id[0],id[1],id[2],round( float( evt[4]), -1),round( float( evt[5]), 0),round( float( evt[6]), 0)))
+     cecile_events.add((channel_map[evt[0]],id[0],id[1],id[2]))
+
+print len(cecile_events)
+#print cecile_events
 
 common_events = open("common_events.txt", 'w')
 my_extras = open("my_extra_events.txt", 'w')
@@ -63,39 +49,32 @@ ceciles_extras = open("ceciles_extra_events.txt", 'w')
 nCommon = 0.0
 nMyExtra = 0.0
 nPExtra = 0.0
-#print my_events
-#print "blah"
-#print cecile_events
+
 common_events_set = my_events.intersection(cecile_events)
 my_extra_set = my_events.difference(cecile_events)
 ceciles_extra_set = cecile_events.difference(my_events)
 
-
-#print len(cecile_events)
-
 for val in common_events_set:
-    #if not (len(val) == 4): continue
-    #s = val[0] + " " + val[1] + " " + val[2] + "\n"
-    #s = val[0] + " " + val[1] + " " + val[2] + " " + val[3] + "\n"
-    s = val[0] + " " + val[1] + " " + val[2] + " " + val[3] + " " + val[4] + " " + val[5] + " " + val[6] +  " " + val[7] + "\n" 
-    #s = val[0] + " " + val[1] + " " + val[2] + " " + val[3] + " " + val[4] + " " + val[5] + " " + val[6] + "\n" # w/o EventID 
-    #print val
+    s = ''
+    for i in range( len( val ) ):
+        s += str(val[i]) + " "
+    s += "\n"
     nCommon+=1
     common_events.write(s)
 
 for val in my_extra_set:
-    #s = val[0] + " " + val[1] + " " + val[2] + "\n"
-    #s = val[0] + " " + val[1] + " " + val[2] + " " + val[3] + "\n"
-    s = val[0] + " " + val[1] + " " + val[2] + " " + val[3] + " " + val[4] + " " + val[5] + " " + val[6] +  " " + val[7] + "\n" 
-    #s = val[0] + " " + val[1] + " " + val[2] + " " + val[3] + " " + val[4] + " " + val[5] + " " + val[6] + "\n" # w/o EventID 
+    s = ''
+    for i in range( len( val ) ):
+        s += str(val[i]) + " "
+    s += "\n"
     nMyExtra+=1
     my_extras.write(s)
 
 for val in ceciles_extra_set:
-    #s = val[0] + " " + val[1] + " " + val[2] + "\n"
-    #s = val[0] + " " + val[1] + " " + val[2] + " " + val[3] + "\n"
-    s = val[0] + " " + val[1] + " " + val[2] + " " + val[3] + " " + val[4] + " " + val[5] + " " + val[6] +  " " + val[7] + "\n" 
-    #s = val[0] + " " + val[1] + " " + val[2] + " " + val[3] + " " + val[4] + " " + val[5] + " " + val[6] + "\n" # w/o EventID 
+    s = ''
+    for i in range( len( val ) ):
+        s += str(val[i]) + " "
+    s += "\n"
     nPExtra+=1
     ceciles_extras.write(s)
 
